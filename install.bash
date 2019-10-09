5e5b0de05ec3d788bd9e17f0c269b3ab41aa84e6
Loic Barrault <loic.barrault@gmail.com>
2019-10-09 11:39:42 +0100

    key bindings

--- install.bash
+++ install.bash
@@ -5,6 +5,7 @@ echo "CWD=$CWD"
 
 ln -s $CWD/bash/.bashrc ~/.bashrc
 ln -s $CWD/bash/.bash_aliases ~/.bash_aliases
+ln -s $CWD/bash/.bash_key_bindings ~/.bash_key_bindings
 ln -s $CWD/bash/.ls_colors ~/.ls_colors
 
 if [ $# -gt 0 ]; then
